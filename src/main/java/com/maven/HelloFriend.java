5d7676f4c002ca3031a7dafd6177c949e0a7bc05
gjk <guojinkun@foxmail.com>
2022-03-30 10:56:13 +0800

    hot fix

--- src/main/java/com/maven/HelloFriend.java
+++ src/main/java/com/maven/HelloFriend.java
@@ -2,6 +2,7 @@ package com.maven;
 
 public class HelloFriend {
 	public String sayHelloToFriend(String name){
+		System.out.println("hot fix...");
 		return "Hello " + name + "! I am " + this.getMyName();
 	}
 	public String getMyName(){
